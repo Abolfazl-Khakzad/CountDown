be2223355ad5dcdc3cdf3fbf137961054975657d
abolfazl <a.khakzad1993@gmail.com>
2022-09-13 08:32:43 +0200

    timer

--- src/Timer.js
+++ src/Timer.js
@@ -7,42 +7,69 @@ class Timer extends React.Component {
         super();
         this.state={
        
-             basicNumber:100
+             hour: 0 ,
+             minute: 0 ,
+             second: 0 ,
+             isStart: false
         }
       }
 
       startInterval= ()=>{
-        interval = setInterval(() => {
+        if(this.state.isStart===false){
             this.setState({
-                basicNumber : this.state.basicNumber -1
+                isStart : true
             })
-        }, 1000);
+            interval = setInterval(() => {
+                this.setState({
+                    second : this.state.second +1
+                })
+                if(this.state.second ===60){
+                    this.setState({
+                        second: 0 ,
+                        minute: this.state.minute +1
+                    })
+                }
+                if(this.state.minute ===60){
+                    this.setState({
+                        minute: 0 ,
+                        hour: this.state.hour +1
+                    })
+                }
+            }, 1000);
+        }
       }
 
       stopInterval= ()=>{
+        this.setState({
+            isStart : false
+        })
         clearInterval(interval);
       }
 
-      componentDidMount(){
-        this.startInterval();
+      resetInterval = ()=>{
+            this.stopInterval();
+            this.setState({
+                hour: 0 ,
+                minute: 0 ,
+                second: 0        
+             })
       }
 
-      componentDidUpdate(){
-        if(this.state.basicNumber === 0){
-               this.stopInterval();
-        }
-      }
      
     render() { 
-        
+        let h = this.state.hour
+        let m = this.state.minute
+        let s = this.state.second
         return (
             <>
                 <h1 className="timer">
-                    {this.state.basicNumber}
+                    {/* {this.state.hour + ":" + this.state.minute + ":" + this.state.second} */}
+                    {`${h >9 ? h : "0"+h} : ${m >9 ? m : "0"+m} : ${s >9 ? s : "0"+s}`}
                 </h1>
                 <div className="buttonBox"> 
                 <button className="startStyle" onClick={this.startInterval}>Start</button>
                 <button className="stopStyle" onClick={this.stopInterval}>Stop</button>
+                <button className="resetStyle" onClick={this.resetInterval}>Reset</button>
                 </div>
             </>
         );
